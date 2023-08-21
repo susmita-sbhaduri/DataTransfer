f23cdab87560c2fe493f91527ae6ad4f8c74b284
sb <sb@sb>
2023-08-21 18:07:07 +0530

    first

---
+++ src/main/java/org/bhaduri/datatransfer/DTO/DataStoreNames.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.bhaduri.datatransfer.DTO;
+
+/**
+ *
+ * @author sb
+ */
+public class DataStoreNames {
+
+    public static final String TICKER_DATA_DETAILS = "/home/sb/tarang_csv/tarang_18Aug.csv";
+//    public static final String INPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls07java.csv";
+    public static final String OUTPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls22aug.csv";
+//    public static final String INPUT_PRICE_DATA_PATH = "/home/sb/Documents/java_testing/price06java.csv";
+    public static final String OUTPUT_MINUTEDATA_INTERVAL = "/home/sb/Documents/java_testing/minDataforInterval.csv";
+    public static final String INPUT_INTRADAY_MINUTEDATA = "/home/sb/Documents/java_testing/minutedata_java.csv";
+    public static final String VALIDATE_SCRIPID = "BPCL";
+    public static final Double DATA_DURATION_SEC = 90.0;
+    public static final String REV_CALL_DATA_SCRIPID = "/home/sb/Documents/java_testing/rev"+VALIDATE_SCRIPID+".csv";
+
+}
