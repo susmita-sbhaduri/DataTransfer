7c52227c7afd3892a1792cfa6e53e7f834c0f1e3
sb <sb@sb>
2023-08-28 17:32:55 +0530

    first

--- src/main/java/org/bhaduri/datatransfer/DTO/ResultData.java
+++ src/main/java/org/bhaduri/datatransfer/DTO/ResultData.java
@@ -2,10 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.bhaduri.generatecall;
-
-import org.bhaduri.datatransfer.DTO.RecordCallPrice;
-import java.util.List;
+package org.bhaduri.datatransfer.DTO;
 
 /**
  *
