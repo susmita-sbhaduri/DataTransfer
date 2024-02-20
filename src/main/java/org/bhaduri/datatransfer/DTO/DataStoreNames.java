eb29ad0dd9a9c94fe58a069bab2cb72767bc657b
sb <sb@sb>
2024-02-20 17:26:42 +0530

    first

--- src/main/java/org/bhaduri/datatransfer/DTO/DataStoreNames.java
+++ src/main/java/org/bhaduri/datatransfer/DTO/DataStoreNames.java
@@ -11,8 +11,8 @@ package org.bhaduri.datatransfer.DTO;
 public class DataStoreNames {//
 
     public static final String TICKER_DATA_DETAILS = "/home/sb/Documents/testi_midday/midday_test/";
-    public static final String TICKER_DATA_NIFTY = "/home/sb/Documents/testi_midday/midday_test/NIFTY 50/NIFTY 50_Feb-15-2024_1.csv";
-    public static final String OUTPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls15feb.csv";
+    public static final String TICKER_DATA_NIFTY = "/home/sb/Documents/testi_midday/midday_test/NIFTY 50/NIFTY 50_Feb-21-2024_1.csv";
+    public static final String OUTPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls21feb.csv";
 //    public static final String INPUT_PRICE_DATA_PATH = "/home/sb/Documents/java_testing/price06java.csv";
     public static final String OUTPUT_MINUTEDATA_INTERVAL = "/home/sb/Documents/java_testing/minDataforInterval.csv";
     public static final String INPUT_INTRADAY_MINUTEDATA = "/home/sb/Documents/java_testing/minutedata_java.csv";
